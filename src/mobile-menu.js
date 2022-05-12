562701be2a919808383ee7c996b41df30a2fdd95
Taras Lapytskyi <84970031+laptar@users.noreply.github.com>
2022-05-12 17:55:14 +0300

    fix-scroll

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -14,7 +14,7 @@
     mobileMenuRef.classList.toggle('is-open');
     tabletMenuRef.classList.toggle('is-open');
 
-    body.classList.toggle('mob-menu__fixed');
+    body.classList.toggle('mob-menu__npmfixed');
     logo.classList.toggle('logo-light');
     logo.classList.toggle('logo-lendscape');
   });
